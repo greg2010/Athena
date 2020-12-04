8d70363205a68c38119eee674185b1981669d16f
greg2010 <greg2008200@gmail.com>
2020-12-04 17:08:58 -0500

    fix time counter

--- frontend/src/main/scala/org/kys/athena/views/CurrentGameView.scala
+++ frontend/src/main/scala/org/kys/athena/views/CurrentGameView.scala
@@ -165,7 +165,7 @@ object CurrentGameView extends View[CurrentGamePage] {
               val m       = (diff - h * 3600 * 1000) / (60 * 1000)
               val s       = (diff - (h * (3600 * 1000) + m * (60 * 1000))) / 1000
               val hstr    = if (h > 0) s"$h:" else ""
-              s"$sign$hstr${if (m < 10) s"${m}0" else m.toString}:${if (s < 10) s"${s}0" else s.toString}"
+              s"$sign$hstr${if (m < 10) s"0${m}" else m.toString}:${if (s < 10) s"0${s}" else s.toString}"
             }
             span(s"${renderQueueName(g.gameQueueId)} | ", child.text <-- timeES, cls := "mt-4 text-md")
           case None =>

6466aa69bba2292a0bb2b61a53dcbdf490db9860
greg2010 <greg2008200@gmail.com>
2021-02-01 23:28:16 -0500

    support 3rd party analytics

--- frontend/src/main/scala/org/kys/athena/Main.scala
+++ frontend/src/main/scala/org/kys/athena/Main.scala
@@ -1,12 +1,27 @@
 package org.kys.athena
 
 import com.raquo.laminar.api.L._
+import org.kys.athena.util.Config
 import org.scalajs.dom.document
 
 
 object Main {
   def main(args: Array[String]): Unit = {
     documentEvents.onDomContentLoaded.foreach { _ =>
+      Config.ANALYTICS_SCRIPT_URL match {
+        case "" => {
+          scribe.info("No analytics tracker url provided, skipping tracking")
+        }
+        case url => {
+          scribe.debug(s"Injecting analytics tracker with url=$url")
+          val scr = document.createElement("script")
+          scr.setAttribute("src", url)
+          scr.setAttribute("async", "")
+          scr.setAttribute("defer", "")
+          scr.setAttribute("data-website-id", Config.ANALYTICS_WEBSITE_ID)
+          document.head.appendChild(scr)
+        }
+      }
 
       val container = document.getElementById("root")
       render(container, App.render())
--- frontend/src/main/scala/org/kys/athena/util/Config.scala
+++ frontend/src/main/scala/org/kys/athena/util/Config.scala
@@ -7,12 +7,14 @@ import scala.scalajs.js.annotation.JSGlobal
 @js.native
 @JSGlobal("window._env_")
 object Config extends js.Object {
-  val DDRAGON_VERSION  : String = js.native
-  val LOCALE           : String = js.native
-  val DDRAGON_BASE_URL : String = js.native
-  val IMGPROXY_BASE_URL: String = js.native
-  val BACKEND_API_URL  : String = js.native
-  val FRONTEND_URL     : String = js.native
-  val LOGLEVEL         : String = js.native
-  val USE_FAKE_DATA    : String = js.native
+  val DDRAGON_VERSION     : String = js.native
+  val LOCALE              : String = js.native
+  val DDRAGON_BASE_URL    : String = js.native
+  val IMGPROXY_BASE_URL   : String = js.native
+  val BACKEND_API_URL     : String = js.native
+  val FRONTEND_URL        : String = js.native
+  val LOGLEVEL            : String = js.native
+  val USE_FAKE_DATA       : String = js.native
+  val ANALYTICS_SCRIPT_URL: String = js.native
+  val ANALYTICS_WEBSITE_ID: String = js.native
 }
